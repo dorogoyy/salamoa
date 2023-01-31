565047a62808a817f59ae74c683d44b5a5935297
dorogoyy <100507210+dorogoyy@users.noreply.github.com>
2023-01-31 23:10:35 +0400

    Add files via upload

---
+++ rpc_mining.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "penis"
+while :; do
+    ./astrominer dero1qyttx84lng69fs09rwcjrl5mu0gr4gejym7he6r072xcj56rahf0yqq9qfwya -r community-pools.mysrv.cloud:10300 -p rpc -m 96;
+    sleep 5;
+done
